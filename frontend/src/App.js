5c6bb73ae0d409dd5090626b970fa9e41f11b0c2
dhiraj-virani <dhiraj_virani16@yahoo.com>
2022-03-30 01:25:23 -0400

    product page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <div>
-      <h1>Lets Get started</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
