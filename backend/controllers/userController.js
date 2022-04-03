733ed608056b95c1a24ff8095ba5870e058835ec
dhiraj-virani <dhiraj_virani16@yahoo.com>
2022-04-03 00:13:04 -0400

    Backend Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,24 +25,58 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register User
+// @route   Post /api/users/
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // @desc    Get user profile
 // @route   Get /api/users/profile
 // @access  Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-        res.json({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-        });
-      } else {
-        res.status(401);
-        throw new Error("User not found");
-      }
-  });
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("User not found");
+  }
+});
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
