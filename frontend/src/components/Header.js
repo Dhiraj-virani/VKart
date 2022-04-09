09b9770b5da09a5a5f23dfbc5e4a131f0d2ffe6f
Dhiraj-virani <100647739+Dhiraj-virani@users.noreply.github.com>
2022-04-09 10:48:21 -0400

    Added Route to home

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,16 +5,18 @@ import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../actions/userActions'
 import SearchBox from './SearchBox'
+import { useHistory } from 'react-router-dom'
+
 
 const Header = () => {
   const dispatch = useDispatch()
-
+  const history = useHistory()
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
   const logoutHandler = () => {
     dispatch(logout())
-    console.log('History reached')
+    history.push('/')
     
   }
   return (
